b39d915b2b759635a50f38172c14b41af7fa3ec3
RomaVintonyak <19roman23@gmail.com>
2020-01-27 18:01:51 +0200

    create burger with flex box menu

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ jQuery(document).ready(function () {
     $(".icon").on("click", function () {
        $(this).closest(".menu").toggleClass("menu-open");
     });
+    /*nav with flex box*/
+    $('.mobile-tab').hide();
+    $('#burg').on("click", function () {
+        $('.mobile-tab').slideToggle(1000);
+    });
 });
